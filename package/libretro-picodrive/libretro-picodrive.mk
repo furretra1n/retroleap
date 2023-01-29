849657cda0d02a3114df5f243c75c2a5a7939b45
mac2612 <mac2612>
2023-01-29 16:52:14 -0700

    Bump picodrive version and fix build

--- package/libretro-picodrive/libretro-picodrive.mk
+++ package/libretro-picodrive/libretro-picodrive.mk
@@ -3,48 +3,26 @@
 # PICODRIVE
 #
 ################################################################################
-LIBRETRO_PICODRIVE_VERSION = d6be4fa64216d73a4bdbf8aa9a31b3917fd82ad1
-LIBRETRO_PICODRIVE_SITE = $(call github,libretro,picodrive,$(LIBRETRO_PICODRIVE_VERSION))
-LIBRETRO_PICODRIVE_DEPENDENCIES = libpng sdl
 
-define LIBRETRO_PICODRIVE_CONFIGURE_CMDS
-	rm -rf $(@D)/picodrive
-	git -C $(@D) clone https://github.com/libretro/picodrive
-	git -C $(@D)/picodrive submodule update --init
-endef
+LIBRETRO_PICODRIVE_VERSION = 80d31d727d45be13813920078259c6dce7973f42
+LIBRETRO_PICODRIVE_SITE = https://github.com/libretro/picodrive.git
+LIBRETRO_PICODRIVE_SITE_METHOD = git
+LIBRETRO_PICODRIVE_DEPENDENCIES = libpng retroarch
+LIBRETRO_PICODRIVE_GIT_SUBMODULES=y
 
-PICOPLATFORM=$(LIBRETRO_PLATFORM)
-
-# Leapster Explorer/Pollux
-ifeq ($(BR2_arm926t),y)
-  PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-endif
-
-# RPI 0 and 1
-ifeq ($(BR2_arm1176jzf_s),y)
-  PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-endif
-
-# RPI 2 and 3
-ifeq ($(BR2_cortex_a7),y)
-  PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-endif
-ifeq ($(BR2_cortex_a8),y)
-  PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-endif
-
-# odroid xu4
-ifeq ($(BR2_cortex_a15),y)
-  PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-endif
+PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
 
 define LIBRETRO_PICODRIVE_BUILD_CMDS
-	$(MAKE) -C $(@D)/picodrive/cpu/cyclone CONFIG_FILE=$(@D)/picodrive/cpu/cyclone_config.h
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" -C  $(@D)/picodrive -f Makefile.libretro platform="$(PICOPLATFORM)"
+	$(MAKE) -C $(@D)/cpu/cyclone CONFIG_FILE=$(@D)/cpu/cyclone_config.h
+	$(SED) "s|-O2|-O3|g" $(@D)/Makefile.libretro
+	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO) -I $(@D)" \
+		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
+		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
+		$(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" -C $(@D)/ -f Makefile.libretro platform="$(PICOPLATFORM)"
 endef
 
 define LIBRETRO_PICODRIVE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/picodrive/picodrive_libretro.so \
+	$(INSTALL) -D $(@D)/picodrive_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/picodrive_libretro.so
 endef
 
