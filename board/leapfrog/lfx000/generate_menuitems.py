3829cb6bf4574d60da5209db3ca5704168ac7f39
mac2612 <mac2612@foo.com>
2023-02-19 21:56:26 -0700

    Add cores and alphabetize

--- board/leapfrog/lfx000/generate_menuitems.py
+++ board/leapfrog/lfx000/generate_menuitems.py
@@ -12,12 +12,19 @@ bin_path = "/usr/bin/"
 
 apps = [
   # Libretro cores. gmenunx will detect these and run retroarch appropriately.
-  App("emulators", "QuickNES", "NES Emulator",  f"{core_path}quicknes_libretro.so", ".nes,.zip", 'fceux.png', "true"),
+  App("emulators", "Atari800", "Atari 800 Emulator", f"{core_path}atari800_libretro.so", ".zip", "gbc.png", "true"), #TODO: Add extensions
+  App("emulators", "CATSFC", "SNES Emulator", f"{core_path}catsfc_libretro.so", ".smc,.zip", 'pocketsnes.png', "true"),
+  App("emulators", "FCEUmm", "NES Emulator", f"{core_path}fceumm_libretro.so", ".nes,.zip", "fceux.png", "true"),
+  App("emulators", "gpsp", "GBA Emulator", f"{core_path}gpsp_libretro.so", ".gba,.zip", "gbc.png", "true"),
+  App("emulators", "imame", "MAME Emulator", f"{core_path}imame4all_libretro.so", ".zip", "mame4all.png", "true"), #TODO: Add extensions
+  App("emulators", "PicoDrive", "MegaDrive/Genesis Emulator", f"{core_path}picodrive_libretro.so", ".md,.zip", "picodrive.png", "true"),
   App("emulators", "PocketSNES", "SNES Emulator", f"{core_path}pocketsnes_libretro.so", ".smc,.zip", 'pocketsnes.png', "true"),
+  App("emulators", "QuickNES", "NES Emulator",  f"{core_path}quicknes_libretro.so", ".nes,.zip", 'fceux.png', "true"),
+
   # Standalone games (lf1000)
-  App("games", "OhBoy", "Gameboy Emulator",  f"{bin_path}ohboy", ".nes,.zip", 'ohboy.png', "true"),
+  App("games", "OhBoy", "Gameboy Emulator",  f"{bin_path}ohboy", ".zip,.gb,.gbc", 'ohboy.png', "true"),
+  App("games", "PicoDrive", "MegaDrive/Genesis Emulator (standalone)", f"{bin_path}PicoDrive", ".md,.zip", "picodrive.png", "true"),
   App("games", "PocketSNES", "SNES Emulator (standalone)", f"{bin_path}PocketSNES", ".smc,.zip", "pocketsnes.png", "true"),
-  App("games", "PicoDrive", "MegaDrive/Genesis Emulator (standalone)", f"{bin_path}PicoDrive", ".smc,.zip", "picodrive.png", "true"),
   App("games", "Retroarch", "Retroarch (main menu)", f"{bin_path}retroarch", "", "generic.png", "false")
 ]
 
