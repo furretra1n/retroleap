77a0d8fdc1f1668fb65c02554148c48174db6ae4
mac2612 <mac2612@foo.com>
2023-03-01 22:25:16 -0700

    (mostly) fix ohboy config issues

--- package/ohboy/ohboy.mk
+++ package/ohboy/ohboy.mk
@@ -13,9 +13,9 @@ OHBOY_DEPENDENCIES = sdl sdl_image sdl_ttf
 OHBOY_INSTALL_STAGING = NO
 OHBOY_GIT_SUBMODULES = YES
 
-
+# DINGOO_OPENDINGUX sets us up to read configs from the HOME directory instead of the working directory.
 define OHBOY_BUILD_CMDS
-        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" ARCH=arm -C $(@D) -f Makefile.linux_sdl all
+        CFLAGS="$(TARGET_CFLAGS) -DDINGOO_OPENDINGUX" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" ARCH=arm -C $(@D) -f Makefile.linux_sdl all
 endef
 
 define OHBOY_INSTALL_TARGET_CMDS
