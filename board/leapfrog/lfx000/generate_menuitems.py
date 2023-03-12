0ae193e783fd60a1edf8dc61b48c263f613f6b9e
mac2612 <mac2612@foo.bar>
2023-03-11 23:42:22 -0500

    Fix annoying bug with file selector

--- board/leapfrog/lfx000/generate_menuitems.py
+++ board/leapfrog/lfx000/generate_menuitems.py
@@ -12,21 +12,24 @@ bin_path = "/usr/bin/"
 
 apps = [
   # Libretro cores. gmenunx will detect these and run retroarch appropriately.
-  App("emulators", "Atari800", "Atari 800 Emulator", f"{core_path}atari800_libretro.so", ".atr,.atx,.pro,.dcm,.xfd,.rom,.bin,.car,.zip", "gbc.png", "true"),
-  App("emulators", "CATSFC", "SNES Emulator", f"{core_path}catsfc_libretro.so", ".smc,.zip", 'pocketsnes.png', "true"),
-  App("emulators", "FCEUmm", "NES Emulator", f"{core_path}fceumm_libretro.so", ".nes,.zip", "fceux.png", "true"),
-  App("emulators", "gpsp", "GBA Emulator", f"{core_path}gpsp_libretro.so", ".gba,.zip", "gbc.png", "true"),
-  App("emulators", "imame", "MAME Emulator", f"{core_path}imame4all_libretro.so", ".zip", "mame4all.png", "true"),
-  App("emulators", "PicoDrive", "MegaDrive/Genesis Emulator", f"{core_path}picodrive_libretro.so", ".md,.zip", "picodrive.png", "true"),
-  App("emulators", "PocketSNES", "SNES Emulator", f"{core_path}pocketsnes_libretro.so", ".smc,.zip", 'pocketsnes.png', "true"),
-  App("emulators", "QuickNES", "NES Emulator",  f"{core_path}quicknes_libretro.so", ".nes,.zip", 'fceux.png', "true"),
+  App("emulators", "Atari800", "Atari 800 Emulator", f"{core_path}atari800_libretro.so", ".atr,.atx,.pro,.dcm,.xfd,.rom,.bin,.car,.zip", "gbc.png", True),
+  App("emulators", "CATSFC", "SNES Emulator", f"{core_path}catsfc_libretro.so", ".smc,.zip", 'pocketsnes.png', True),
+  App("emulators", "FCEUmm", "NES Emulator", f"{core_path}fceumm_libretro.so", ".nes,.zip", "fceux.png", True),
+  App("emulators", "gpsp", "GBA Emulator", f"{core_path}gpsp_libretro.so", ".gba,.zip", "gbc.png", True),
+  App("emulators", "imame", "MAME Emulator", f"{core_path}imame4all_libretro.so", ".zip", "mame4all.png", True),
+  App("emulators", "PicoDrive", "MegaDrive/Genesis Emulator", f"{core_path}picodrive_libretro.so", ".md,.zip", "picodrive.png", True),
+  App("emulators", "PocketSNES", "SNES Emulator", f"{core_path}pocketsnes_libretro.so", ".smc,.zip", 'pocketsnes.png', True),
+  App("emulators", "QuickNES", "NES Emulator",  f"{core_path}quicknes_libretro.so", ".nes,.zip", 'fceux.png', True),
 
   # Standalone games (lf1000)
-  App("games", "OhBoy", "Gameboy Emulator",  f"{bin_path}ohboy", ".zip,.gb,.gbc", 'ohboy.png', "true"),
-  App("games", "PicoDrive", "MegaDrive/Genesis Emulator (standalone)", f"{bin_path}PicoDrive", ".md,.zip", "picodrive.png", "true"),
-  App("games", "PocketSNES", "SNES Emulator (standalone)", f"{bin_path}PocketSNES", ".smc,.zip", "pocketsnes.png", "true"),
-  App("games", "Retroarch", "Retroarch (main menu)", f"{bin_path}retroarch", "", "generic.png", "false"),
-  App("games", "snes9x4d", "SNES emulator (standalone)", f"{bin_path}snes9x4d", ".smc,.zip", "pocketsnes.png", "true")
+  App("games", "OhBoy", "Gameboy Emulator",  f"{bin_path}ohboy", ".zip,.gb,.gbc", 'ohboy.png', True),
+  App("games", "PicoDrive", "MegaDrive/Genesis Emulator (standalone)", f"{bin_path}PicoDrive", ".md,.zip", "picodrive.png", True),
+  App("games", "PocketSNES", "SNES Emulator (standalone)", f"{bin_path}PocketSNES", ".smc,.zip", "pocketsnes.png", True),
+  App("games", "Retroarch", "Retroarch (main menu)", f"{bin_path}retroarch", "", "generic.png", False),
+  App("games", "snes9x4d", "SNES emulator (standalone)", f"{bin_path}snes9x4d", ".smc,.zip", "pocketsnes.png", True),
+
+  # Applications (terminal, etc)
+  App("applications", "st-sdl", "Terminal Emulator", f"{bin_path}st", "", "terminal.png", False)
 ]
 
 buildroot_root = sys.argv[1]
@@ -43,8 +46,11 @@ icon=skin:icons/{a.icon}
 exec={a.path}
 volume=1
 wrapper=true
-selectordir=/roms/
-selectorfilter={a.extensions}
-selectorbrowser={a.selectorbrowser}
 '''
         )
+        if a.selectorbrowser:
+            f.write(
+                f'''selectordir=/roms/
+selectorfilter={a.extensions}
+selectorbrowser=true
+''')
