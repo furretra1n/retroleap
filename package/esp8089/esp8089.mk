551fd575b518147118a26a3c52e97ff4a2eefd5c
mac2612 <mac2612@foo.com>
2023-04-25 21:34:20 -0600

    Bump esp8089 version and add patch for lf1000

--- package/esp8089/esp8089.mk
+++ package/esp8089/esp8089.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ESP8089_VERSION = 58918e272f22da212045f6280c454085456ca1c0
+ESP8089_VERSION = 0f61a8d29368d988fe0254d79474246ca39e997e
 ESP8089_SITE = https://github.com/al177/esp8089.git
 ESP8089_SITE_METHOD = git
 ESP8089_LICENSE = GPL-2.0
